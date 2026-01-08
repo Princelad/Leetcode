b75e203eaea17a1c22c718eae7f97757340a5bb0
Prince Lad <ladprince1112@gmail.com>
2026-01-08 10:53:49 +0530

    Refactor

---
+++ 6. Zigzag Conversion.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> rows(numRows, "");
+        int size = s.size();
+
+        for (int i = 0; i < size; ++i) {
+            
+        }
+    }
+};
